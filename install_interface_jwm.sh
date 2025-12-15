091c3f673df3845c4a4418ff03ce41e644dd9958
antonieloficial <antonieloficial@gmail.com>
2025-12-15 01:12:19 -0300

    Update install_interface_jwm.sh

--- install_interface_jwm.sh
+++ install_interface_jwm.sh
@@ -209,6 +209,13 @@ chmod +x ~/startvnc
 sudo mkdir -p /usr/share/backgrounds
 sudo sh -c 'echo "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg==" | base64 -d > /usr/share/backgrounds/default.png'
 
+# Instalar o Wine
+sudo dpkg --add-architecture i386
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+sudo apt install --install-recommends winehq-stable -y
+
 # ============================================
 # 5. CRIAR SCRIPTS DE CONTROLE
 # ============================================
@@ -271,6 +278,9 @@ echo "   Senha: 123456"
 echo ""
 echo "📊 STATUS: ~/jwm-status"
 echo "📁 ABRIR ARQUIVOS: ~/open-files"
-
+echo ""
+echo "🍷  CONFIGURAR WINE"
+echo "    winecfg"
 echo "=========================================="
+
 
